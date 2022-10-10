bd69c5cb49cb4ae4871e7bd3f9085c4db6531a50
Joprocorp <joprocorp@gmail.com>
2022-10-10 11:24:46 +0200

    feat: Api data fetching and display to Table component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import ApiService from "../services/ApiService";
+import Table from "./Table";
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  let [posts, setPosts] = useState(undefined)
+
+  useEffect(() => {
+    setTimeout(() => {
+      ApiService.get("/api/posts", (json) => {
+        setPosts(json.posts.map((post) => {
+          return {
+            "Titre": post.title,
+            "Publié le": post.publishDate,
+            "Résumé": post.summary.substring(0, Math.min(post.summary.length, 200) + (post.summary.length > 200 ?? "...")),
+            "Auteur": post.author.name,
+            "Catégories": post.categories.map((c) => c.name).join(", ")
+          }
+        }))
+      })
+    }, Math.random() * 1000);
+  }, [])
+
+  return <div>
+    {
+      posts === undefined ?
+      "Please wait a moment..." :
+      <Table data={posts}></Table>
+    }
+  </div>;
 }
 
 export default App;
