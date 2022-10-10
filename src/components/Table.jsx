da446927f2b6b84a8c5a0834bc6e6c33e4e39429
Joprocorp <joprocorp@gmail.com>
2022-10-10 22:17:11 +0200

    feat: Adding pagination

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,10 +6,14 @@ import "./Table.css"
  * @param {JSON object array} props.data: An array of data objects (level 1 JSON object) with all the same properties
  * @returns JSX
  */
+
 function Table(props) {
     let [orderedBy, setOrderedBy] = useState(undefined) // Ordering settings
     let template = assertDataFormat(props.data) // Data properties as keys, types and uniqueValues (for Arrays) as values. See function for more info.
     let properties = Object.keys(template) // Data properties
+    let [itemPerPage, setItemPerPage] = useState(10)
+    let [maxPage, setMaxPage] = useState(Math.ceil(props.data.length / itemPerPage))
+    let [page, setPage] = useState(1)
 
     // Filter parameter
     let _filter = {}
@@ -21,6 +25,19 @@ function Table(props) {
     // Adding a hook to monitor changes in filter
     let [filter, setFilter] = useState(_filter)
 
+
+
+    useEffect(() => {
+        let nbItems = getSortedFilteredData().length
+        let newMaxPage = Math.ceil(nbItems / itemPerPage)
+        setMaxPage(newMaxPage)
+        if (page > newMaxPage) {
+            setPage(Math.max(newMaxPage, 1))
+        }
+    }, [filter, itemPerPage])
+
+
+
     /**
      * Handle a click on the ordering button of a column (property)
      */
@@ -31,7 +48,7 @@ function Table(props) {
                 asc: true
             })
         } else { // If the user clicked on the ordering button of the currently ordered property (by it ascending or not)
-            if(orderedBy.asc === true){ // the ordering is set to ascending
+            if (orderedBy.asc === true) { // the ordering is set to ascending
                 setOrderedBy({ // set to descending
                     property: property,
                     asc: false
@@ -51,6 +68,69 @@ function Table(props) {
         setFilter(_filter)
     }
 
+    const clickedPage = function (page) {
+        setPage(page)
+    }
+
+    const getSortedFilteredData = function () {
+        return [...props.data].sort((a, b) => { // copy the data given to the component to a new object reference
+            // sorts the objects depending on the ordering state
+            if (orderedBy === undefined) return 0
+            if (orderedBy.asc) {
+                let t = a
+                a = b
+                b = t
+            }
+
+            return a[orderedBy.property].localeCompare(b[orderedBy.property])
+        }).filter((o) => { // filters the sorted rows depending on the filtering state
+
+            // For each property in the template
+            for (let j = 0; j < Object.entries(template).length; j++) {
+                const [property_name, property_info] = Object.entries(template)[j];
+                const filter_value = filter[property_name];
+
+                // if array
+                if (property_info.type === "array") {
+                    // Checks the row's property value contains ALL selected values in the filter (multielement selector)
+                    for (let i = 0; i < filter[property_name].length; i++) {
+                        const filterValue = filter[property_name][i];
+                        if (!(o[property_name].includes(filterValue))) {
+                            return false // will be filtered OUT
+                        }
+                    }
+                } else {
+                    if (filter_value === "") continue
+                    if (!(o[property_name].toLowerCase().includes(filter_value.toLowerCase()))) { // if the property value does not contain the property's filter value
+                        return false // will be filtered OUT
+                    }
+                }
+            }
+            return true // will be kept
+        })
+    }
+
+    let paginationFirst = []
+    let pagFirstMin = 1
+    let pagFirstMax = Math.min(maxPage, 3)
+    for (let i = pagFirstMin; i <= pagFirstMax; i++) {
+        paginationFirst.push(<button onClick={(e) => { clickedPage(i) }} disabled={page === i}>{i}</button>)
+    }
+
+    let paginationMiddle = []
+    let pagMiddleMin = Math.max(4, page - 1)
+    let pagMiddleMax = Math.min(maxPage - 2, page + 2)
+    for (let i = pagMiddleMin; i < pagMiddleMax; i++) {
+        paginationMiddle.push(<button onClick={(e) => { clickedPage(i) }} disabled={page === i}>{i}</button>)
+    }
+
+    let paginationLast = []
+    let pagLastMin = Math.max(maxPage - 2, 4)
+    let pagLastMax = maxPage
+    for (let i = pagLastMin; i <= pagLastMax; i++) {
+        paginationLast.push(<button onClick={(e) => { clickedPage(i) }} disabled={page === i}>{i}</button>)
+    }
+
     return (
         <div>
             <div>
@@ -62,22 +142,23 @@ function Table(props) {
                                 <span>{property_name}: </span>
                                 {
                                     property_info.type !== "array"
-                                    // if the property is NOT an array, display a normal text input
-                                    ? <input type="text" name={property_name} value={filter[property_name]} onChange={(e) => {
-                                        // When changing the input value, calls the handling function with the property name and the new value
-                                        changedFilterValue(property_name, e.target.value) }
-                                    } />
-                                    // else, display a multielement selector with the uniqueValues as options
-                                    : (
-                                        <select name="property" onChange={(e) => {
-                                            // When changing the options, calls the handling function with the property name and the selected options
-                                            changedFilterValue(property_name, [...e.target.options].filter((v) => v.selected).map((v) => v.value))
-                                        }} multiple>
-                                            {property_info.uniqueValues.map((v) => {
-                                                return <option value={v}>{v}</option>
-                                            })}
-                                        </select>
-                                    )
+                                        // if the property is NOT an array, display a normal text input
+                                        ? <input type="text" name={property_name} value={filter[property_name]} onChange={(e) => {
+                                            // When changing the input value, calls the handling function with the property name and the new value
+                                            changedFilterValue(property_name, e.target.value)
+                                        }
+                                        } />
+                                        // else, display a multielement selector with the uniqueValues as options
+                                        : (
+                                            <select name="property" onChange={(e) => {
+                                                // When changing the options, calls the handling function with the property name and the selected options
+                                                changedFilterValue(property_name, [...e.target.options].filter((v) => v.selected).map((v) => v.value))
+                                            }} multiple>
+                                                {property_info.uniqueValues.map((v) => {
+                                                    return <option value={v}>{v}</option>
+                                                })}
+                                            </select>
+                                        )
                                 }
                             </label>
                         </div>
@@ -93,8 +174,8 @@ function Table(props) {
                                     {property_name}
                                     <button onClick={(e) => { clickedOrderBtn(property_name) /** Calls the handling function for the ordering button with the name of the property */ }}>
                                         {
-                                        // displays a button with arrow depending on the ordering state
-                                        orderedBy !== undefined && orderedBy.property === property_name ? (orderedBy.asc ? "🠗" : "🠕") : "⇵"
+                                            // displays a button with arrow depending on the ordering state
+                                            orderedBy !== undefined && orderedBy.property === property_name ? (orderedBy.asc ? "🠗" : "🠕") : "⇵"
                                         }
                                     </button>
                                 </th>
@@ -102,54 +183,43 @@ function Table(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        {[...props.data].sort((a, b) => { // copy the data given to the component to a new object reference
-                            // sorts the objects depending on the ordering state
-                            if (orderedBy === undefined) return 0
-                            if (orderedBy.asc) {
-                                let t = a
-                                a = b
-                                b = t
-                            }
-
-                            return a[orderedBy.property].localeCompare(b[orderedBy.property])
-                        }).filter((o) => { // filters the sorted rows depending on the filtering state
-
-                            // For each property in the template
-                            for (let j = 0; j < Object.entries(template).length; j++) {
-                                const [property_name, property_info] = Object.entries(template)[j];
-                                const filter_value = filter[property_name];
-
-                                // if array
-                                if(property_info.type === "array"){
-                                    // Checks the row's property value contains ALL selected values in the filter (multielement selector)
-                                    for (let i = 0; i < filter[property_name].length; i++) {
-                                        const filterValue = filter[property_name][i];
-                                        if(!(o[property_name].includes(filterValue))){
-                                            return false // will be filtered OUT
-                                        }
-                                    }
-                                } else {
-                                    if (filter_value === "") continue
-                                    if (!(o[property_name].toLowerCase().includes(filter_value.toLowerCase()))) { // if the property value does not contain the property's filter value
-                                        return false // will be filtered OUT
-                                    }
-                                }
-                            }
-                            return true // will be kept
-                        }).map((o) => {
+                        {getSortedFilteredData().slice((page - 1) * itemPerPage, page * itemPerPage).map((o) => {
                             // Formats the data to be displayed as a row in the table
                             return <tr>
                                 {Object.entries(template).map(([property_name, property_info]) => {
-                                    if(property_info.type === "array"){
-                                        return <td>{o[property_name].join(", ")}</td>
+                                    if (property_info.type === "array") {
+                                        return <td><div className="limit-text-2">{o[property_name].join(", ")}</div></td>
                                     } else {
-                                        return <td>{o[property_name]}</td>
+                                        let colored = o[property_name]
+                                        if(filter[property_name] !== ""){
+                                            colored = colored.replace(new RegExp(filter[property_name], "gi"), `<font color="red">${filter[property_name]}</font>`)
+                                        }
+                                        return <td><div className="limit-text-2" dangerouslySetInnerHTML={{__html: colored}}></div></td>
                                     }
                                 })}
                             </tr>
                         })}
                     </tbody>
                 </table>
+            </div>
+            <div>
+                <center>
+
+                    <select onChange={(e) => {
+                        setItemPerPage(e.target.value)
+                    }}>
+                        <option value={1}>1</option>
+                        <option value={5}>5</option>
+                        <option value={10} selected>10</option>
+                        <option value={20}>20</option>
+                        <option value={50}>50</option>
+                        <option value={100}>100</option>
+                    </select>
+
+                    <button onClick={(e) => { setPage(page - 1) }} disabled={page === 1}>🠔</button>
+                    {paginationFirst}{pagFirstMax + 1 === pagMiddleMin ? "" : "..."}{paginationMiddle}{pagMiddleMax + 1 === pagLastMin ? "" : "..."}{paginationLast}
+                    <button onClick={(e) => { setPage(page + 1) }} disabled={page === maxPage}>➝</button>
+                </center>
             </div>
         </div>
     )
@@ -167,7 +237,7 @@ function assertDataFormat(data) {
     // Gets template properties from first data object in array
     const template_properties = Object.keys(data[0]) // Takes the JSON keys of the first data object
     const template_types = Object.entries(data[0]).map(([property, v]) => { // For each property (and its value) of the first data object
-        if (Array.isArray(v)){ // if the property's value of the template is an array itself
+        if (Array.isArray(v)) { // if the property's value of the template is an array itself
             let uniqueValues = [] // init
             for (let i = 0; i < data.length; i++) { // for every row
                 const element_value = data[i][property]; // take the property's value of the row
@@ -176,7 +246,7 @@ function assertDataFormat(data) {
 
             return {
                 type: "array",
-                uniqueValues: uniqueValues.sort((a,b) => a.localeCompare(b))
+                uniqueValues: uniqueValues.sort((a, b) => a.localeCompare(b))
             }
         } else {
             return { type: typeof v }
